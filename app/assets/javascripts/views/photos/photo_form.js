338f4f21cfb5693c5c9cfd1545b4f0d1c69bdb67
Erez Bosch <erezaka@gmail.com>
2015-08-13 09:11:53 -0700

    add preview to addphoto page

--- app/assets/javascripts/views/photos/photo_form.js
+++ app/assets/javascripts/views/photos/photo_form.js
@@ -15,6 +15,12 @@ Bickr.Views.PhotoForm = Backbone.View.extend({
     cloudinary.openUploadWidget(CLOUDINARY_OPTIONS, function (error, result) {
       var data = result[0];
       this.model.set({ image_url: data.url, public_id: data.public_id });
+      var image = $.cloudinary.image(data.public_id, {
+        width: Math.floor(window.innerWidth * (2 / 3)),
+        height: Math.floor(window.innerHeight - 150),
+        crop: 'limit',
+      });
+      this.$('#preview').append(image);
     }.bind(this));
   },
 
