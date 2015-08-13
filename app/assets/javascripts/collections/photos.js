3b95d22432ffe98d9811dd780fb4e6b1d159ab10
Erez Bosch <erezaka@gmail.com>
2015-08-13 13:01:59 -0700

    cleaned up photostream

--- app/assets/javascripts/collections/photos.js
+++ app/assets/javascripts/collections/photos.js
@@ -1,4 +1,7 @@
 Bickr.Collections.Photos = Backbone.FetchableCollection.extend({
   model: Bickr.Models.Photo,
   url: '/api/photos',
+  comparator: function (photo) {
+    return -photo.get('id');
+  },
 });
