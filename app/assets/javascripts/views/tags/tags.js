05a2a9a9dfc7c1efbd2fbf166602a98849c7c24e
Erez Bosch <erezaka@gmail.com>
2015-08-14 12:04:27 -0700

    enabled tag removal in photo/album forms - actually destroys only tagging

--- app/assets/javascripts/views/tags/tags.js
+++ app/assets/javascripts/views/tags/tags.js
@@ -1,11 +1,25 @@
 Bickr.Views.Tags = Backbone.View.extend({
   template: JST['tags/tags'],
 
+  events: {
+    'click .tag-link': 'removeTag',
+  },
+
   initialize: function () {
     this.listenTo(this.collection, 'sync add remove', this.render);
   },
 
   render: function () {
     this.$el.html(this.template({ tags: this.collection }));
+  },
+
+  removeTag: function (e) {
+    var $link = $(e.currentTarget);
+    var targetTag = this.collection.find(function (tag) {
+      return tag.get('label') === $link.text();
+    });
+    this.collection.remove(targetTag);
+    // note we DON'T want to destroy the tag, just the tagging which is done
+    // in rails on form submit
   },
 });
