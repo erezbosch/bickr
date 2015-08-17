ace4a3168a6a8f1b6bc91ab309e72e3d985f72aa
Erez Bosch <erezaka@gmail.com>
2015-08-17 15:43:34 -0700

    dblclick on 'You' dropdown goes to your user page

--- app/assets/javascripts/views/nav/nav.js
+++ app/assets/javascripts/views/nav/nav.js
@@ -2,10 +2,21 @@ Bickr.Views.Nav = Backbone.CompositeView.extend({
   template: JST['nav/nav'],
   tagName: 'header',
 
+  events: {
+    'dblclick .you': 'goToYou',
+  },
+
   initialize: function () {
     this.addSubview(
       '.search-form',
       new Bickr.Views.Search()
+    );
+  },
+
+  goToYou: function () {
+    Backbone.history.navigate(
+      '#/api/users/' + CURRENT_USER.id,
+      { trigger: true }
     );
   },
 
