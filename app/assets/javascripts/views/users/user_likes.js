309f8c7560adf2a63141c9c690aeeed4f541fd44
Erez Bosch <erezaka@gmail.com>
2015-08-17 09:27:37 -0700

    add Bb view for likes

---
+++ app/assets/javascripts/views/users/user_likes.js
@@ -0,0 +1,27 @@
+Bickr.Views.UserLikes = Backbone.CompositeView.extend({
+  template: JST['users/user_likes'],
+
+  initialize: function (options) {
+    this.albums = options.albums;
+    this.photos = options.photos;
+    this.addSubview(
+      ".albums-index",
+      new Bickr.Views.AlbumsIndex({ collection: this.albums })
+    );
+    this.addSubview(
+      ".photos-index",
+      new Bickr.Views.PhotosIndex({
+        collection: this.photos,
+        thumbnail: true,
+        isSubview: true,
+      })
+    );
+    this.listenTo(this.albums, 'sync', this.render);
+    this.listenTo(this.photos, 'sync', this.render);
+  },
+
+  render: function () {
+    this.$el.html(this.template({ albums: this.albums, photos: this.photos, }));
+    this.attachSubviews();
+  },
+});
