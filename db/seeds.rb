9ac8ea3b4d296c2e66211839bb9edc7e6cf154d0
Erez Bosch <erezaka@gmail.com>
2015-08-20 16:57:09 -0700

    begin writing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = Array.new(10) do
+  User.create!({ email: Faker::Internet.safe_email, password: 'password' })
+end
+
+albums = [] # 20 x { title: , description: }
+albums.map! { |album| }
+photos = [] # 100 x { image_url: , public_id: , title: , description: }
+tags = [] # 25 x { label: }
+
+photos.map! do |photo|
+  user = users.sample
+  unless [true, true, false].sample || user.albums.blank?
+    photo = user.photos.create!(photo)
+  else
+    photo = user.albums.sample.photos.create!(photo)
+  end
+end
+
+
+users << User.create!({email: 'guest@example.com', password: 'password' })
