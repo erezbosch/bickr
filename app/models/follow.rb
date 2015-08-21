9e4a1d4a1828f6201968159977651fbb4a988877
Erez Bosch <erezaka@gmail.com>
2015-08-20 17:39:05 -0700

    add can't-follow-yourself validation

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,11 @@
 class Follow < ActiveRecord::Base
   validates :followee, :follower, presence: true
   validates :followee_id, uniqueness: { scope: :follower_id }
+  validate :follower_not_followee
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
+
+  def follower_not_followee
+    errors.add(:users, "can't follow themselves") if follower_id == followee_id
+  end
 end
