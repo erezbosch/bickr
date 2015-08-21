0d0334b090a4277eae934128acf25ca50fc41ef5
Erez Bosch <erezaka@gmail.com>
2015-08-21 09:30:41 -0700

    work on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,31 @@
 albums = [] # 10 x { title: , description: }
 photos = [] # 100 x { image_url: , public_id: , title: , caption: [leave this off for some] }
 tags = [] # 25 x { label: }
-avatars = [] # 8 x cloudinary image urls
+avatars = [
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1440089963/rhf52skfmx94r6jwo8qi.png',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1439945523/pi4d8snzwytzgzyx331x.gif',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1439944477/gaulcveo3rlmszca2pof.jpg',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1439853846/xbdqzxz9fxpugvcipbck.jpg',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1440173888/Astronotus_ocellatus_-_closeup__aka_jwy14g.jpg',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1440173911/Snail_0075_prbzxc.jpg',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1440173926/Toddy_Dog_bunvkt.jpg',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1440173940/Elephant__Loxodonta_Africana__04_fkmv3v.jpg',
+  'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1440174588/eagle-eye-katie-abrams_ds5q6m.jpg'
+] # 8 x cloudinary image urls
 comment_hashes = Array.new(500) {} # some faker things
 
-users = Array.new(10) do |i| # give them avatar_url
-  User.create!(
+users = Array.new(avatars.length * 1.25) do |i| # give them avatar_url
+  User.create(
     email: Faker::Internet.safe_email,
     password: 'password',
-    avatar_url: i < 8 ? avatars[i] : nil
+    avatar_url: i < avatars.length ? avatars[i] : nil
   )
 end
-guest = User.create!(
+users << User.create(
   email: 'guest@example.com',
   password: 'password',
-  avatar_url: '' # make it something
+  avatar_url: 'http://res.cloudinary.com/dbxvb3ap5/image/upload/v1440005771/gknctceka4r9zfrlbng9.jpg'
 )
-users << guest
 
 albums.map! { |album_hash| users.sample.albums.create(album_hash) }
 
@@ -41,14 +50,27 @@ end
 comments = []
 comment_hashes.each do |hash|
   commentable = (comments + albums * 2 + photos * 2).sample
-  comments << commentable.comments.create(hash.merge({ user_id: rand(11) }))
+  comments << commentable.comments.create(hash.merge({
+    user_id: rand(users.length)
+  }))
 end
 
-albums[0...5].each { |album| album.update(cover_photo: album.photos.sample) }
+albums[0...albums.length / 2].each do |album|
+  album.update(cover_photo: album.photos.sample)
+end
 
-300.times { photos.concat(albums).sample.taggings.create(tag_id: rand(25)) }
+(photos.concat(albums).length * 4).times do
+  photos.concat(albums).sample.taggings.create(tag_id: rand(tags.length))
+end
 
-25.times { users.sample.in_follows.create(follower_id: rand(10)) }
-10.times { |i| guest.out_follows.create(followee_id: users[i]) }
+(users.length * 3).times do
+  users.sample.in_follows.create(follower_id: rand(users.length - 1))
+end
 
-500.times { photos.concat(albums).sample.likes.create(user_id: rand(11)) }
+(users.length - 1).times do |i|
+  users.last.out_follows.create(followee_id: users[i])
+end
+
+(photos.concat(albums).length * 4).times do
+  photos.concat(albums).sample.likes.create(user_id: rand(users.length))
+end
