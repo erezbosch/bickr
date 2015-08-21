9f880ebd08e8afe53f3385ebdb7f7b1f0cf9acd7
Erez Bosch <erezaka@gmail.com>
2015-08-20 17:33:12 -0700

    more seed work

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-avatars = Array.new(10)
+
+albums = [] # 10 x { title: , description: }
+photos = [] # 100 x { image_url: , public_id: , title: , caption: [leave this off for some] }
+tags = [] # 25 x { label: }
+avatars = [] # 8 x cloudinary image urls
 
 users = Array.new(10) do |i| # give them avatar_url
   User.create!(
     email: Faker::Internet.safe_email,
     password: 'password',
-    avatar_url: avatars[i]
+    avatar_url: i < 8 ? avatars[i] : nil
   )
 end
 
@@ -22,16 +26,19 @@ guest = User.create!(
   avatar_url: ''
 )
 
-albums = [] # 10 x { title: , description: }
-photos = [] # 100 x { image_url: , public_id: , title: , caption: [leave this off for some] }
-tags = [] # 25 x { label: }
-albums.map! { |album| users.sample.albums.create!()}
+albums.map! { |album_hash| users.sample.albums.create!(album_hash) }
 
-photos.map! do |photo|
+photos.map! do |photo_hash|
   user = users.sample
   unless [true, false, false].sample || user.albums.blank?
-    photo = user.photos.create!(photo)
+    user.photos.create!(photo_hash)
   else
-    photo = user.albums.sample.photos.create!(photo.merge({ uploader: user }))
+    user.albums.sample.photos.create!(photo_hash.merge({ uploader: user }))
   end
 end
+
+albums[0...5].each { |album| album.update(cover_photo: album.photos.sample) }
+
+250.times { photos.concat(albums).sample.taggings.create!(tag_id: rand(25)) }
+
+20.times { users.sample.out_follows.create(followee_id: rand(10)) }
