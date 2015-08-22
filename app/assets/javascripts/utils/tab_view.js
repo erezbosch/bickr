a6fba31abe48f9b5b9da6f546e52e2b984f1e8ea
Erez Bosch <erezaka@gmail.com>
2015-08-22 10:28:15 -0700

    fix dreaded tabs hack using .resize()

--- app/assets/javascripts/utils/tab_view.js
+++ app/assets/javascripts/utils/tab_view.js
@@ -4,14 +4,10 @@ Backbone.TabView = Backbone.CompositeView.extend({
   },
 
   renderTab: function (e) {
-    if (this.renderedTabs) { return; }
-    // reset layout after allowing opacity transition to occur
-    setTimeout(function () {
-      var $targetGrid = this.$($(e.currentTarget).attr('href')).find('.grid');
-      $targetGrid.imagesLoaded(function() {
-        $targetGrid.masonry('layout');
-      });
-      this.renderedTabs = true;
-    }.bind(this), 200);
+    var $targetGrid = this.$($(e.currentTarget).attr('href')).find('.grid');
+    $(window).resize();
+    $targetGrid.imagesLoaded(function () {
+      $targetGrid.masonry('layout');
+    });
   },
 });
